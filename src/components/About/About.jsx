020f6edfc7d2f9e37026110bd46a4d737cbc4109
Xaphan64 <alex_daniel64@yahoo.com>
2024-04-27 19:47:10 +0300

    Update About.jsx

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -24,8 +24,9 @@ const About = () => {
   return (
     <div className="about-container">
       <h2>
-        This is a Blog project made by Alexandru-Daniel Sarbu in React JS with a JSON server. In this project you can
-        create and delete blogs.
+        This is a Blog project made by Alexandru-Daniel Sarbu in React with a JSON server. In this project you can
+        create and delete blogs. Due to the fact that the JSON server is deployed on render.com the API may fetch with a
+        several minutes delay for the first time.
       </h2>
     </div>
   );
